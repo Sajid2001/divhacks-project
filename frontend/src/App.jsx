7aafc40b5892c2761c1add4fd786949dfc80b6c7
ChrisCaliendo <christiantcaliendo@gmail.com>
2023-09-23 15:30:37 -0400

    fff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import Header from'./Header.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Medications from './Medication.jsx';
+import Orders from './Orders.jsx';
+import Home from './Home.jsx';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App" >
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/medications' element={<Medications/>}/>
+            <Route path='/orders' element={<Orders/>}/>
+        </Routes>
+      </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
