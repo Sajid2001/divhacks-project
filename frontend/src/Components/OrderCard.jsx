7e7bfd5f8b2b27396ed4ad8c5c6a1bcaea4ed348
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-09-23 19:10:27 -0400

    added orders

--- frontend/src/Components/OrderCard.jsx
+++ frontend/src/Components/OrderCard.jsx
@@ -8,13 +8,13 @@ export default function OrderCard({order}) {
   direction={{ base: 'column', sm: 'row' }}
   overflow='hidden'
   variant='outline'
-  maxW={'2xl'}
+  minW={'2xl'}
 >
   <Image
     objectFit='cover'
     maxW={{ base: '100%', sm: '200px' }}
-    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-    alt='Caffe Latte'
+    src={order.imageUrl}
+    alt='Drugs'
   />
 
   <Stack>
@@ -23,6 +23,12 @@ export default function OrderCard({order}) {
 
       <Text py='2'>
         {order.description}
+      </Text>
+      <Text py='2'>
+        {order.price}
+      </Text>
+      <Text py='2'>
+        {order.form}
       </Text>
     </CardBody>
 
--- frontend/src/Pages/Orders.jsx
+++ frontend/src/Pages/Orders.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 const Orders = () => {
     
-const [orders, setOrders] = useState([[]]);
+const [orders, setOrders] = useState([]);
 
 const getOrders = async() => {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/orders`)
@@ -28,9 +28,9 @@ useEffect(()=> {
 
             {orders && 
             <Stack justify={'center'} align={'center'} gap={3} marginY={4}>
-                {orders.map(order => {
+                {orders.map(order => (
                     <OrderCard order = {order}/>
-                })}
+                ))}
             </Stack>
             }
         </div>
